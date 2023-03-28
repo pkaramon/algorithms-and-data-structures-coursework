b1b2a3c4aad0791106b74e37b8b50d48aadcc7d1
Piotr Karamon <piotrkaramon3@gmail.com>
2023-03-29 01:05:27 +0200

    created quick_select and magick fives

---
+++ sorting/quick_select.py
@@ -0,0 +1,27 @@
+import random
+
+
+def quick_select(A, left, right, k):
+    while True:
+        q = partition_quick_select(A, left, right)
+        if q == k:
+            return A[k]
+        if k > q:
+            left = q+1
+        else:
+            right = q-1
+
+
+def partition_quick_select(A, left, right):
+    pivot_ind = random.randint(left, right)
+    A[pivot_ind], A[right] = A[right], A[pivot_ind]
+
+    pivot = A[right]
+
+    i = left-1
+    for j in range(left, right):
+        if A[j] < pivot:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[right], A[i+1] = A[i+1], A[right]
+    return i+1
